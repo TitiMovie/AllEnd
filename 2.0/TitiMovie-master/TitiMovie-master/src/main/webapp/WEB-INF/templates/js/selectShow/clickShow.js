c1b9314e8ac9dfbe0f46ab5b7404e9091789d4f6
chryimi <chryimi@users.noreply.github.com>
2017-06-25 19:57:04 +0800

    更新3

--- 2.0/TitiMovie-master/TitiMovie-master/src/main/webapp/WEB-INF/templates/js/selectShow/clickShow.js
+++
@@ -1,7 +0,0 @@
-function clickShow(self) {
-    post('/selectSeat', {
-        theater_name:self.name,
-        show:$(self).text()
-    })
-    "redirect:/selectSeat/" + theater_id + "?movie_id=" + movie_id + "&show_id=" + show;
-}
