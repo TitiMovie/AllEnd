a289b4db3846dd37c8666356b724c904ad50d4de
zzrlzq <zzrlk@foxmail.com>
2017-06-25 19:47:03 +0800

    服务更新2

---
+++ TitiMovie-Server-master/TitiMovie-Server-master/src/main/java/com/movietime/entity/User.java
@@ -0,0 +1,61 @@
+package com.movietime.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String email;
+    private String iconPath;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIconPath() {
+        return iconPath;
+    }
+
+    public void setIconPath(String iconPath) {
+        this.iconPath = iconPath;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ TitiMovie-Server-master/TitiMovie-Server-master/src/main/java/com/movietime/vo/BannerVO.java
@@ -0,0 +1,39 @@
+package com.movietime.vo;
+
+public class BannerVO {
+    private int id;
+    private String name;
+    private String path;
+
+    public BannerVO() {}
+
+    public BannerVO(int id, String name, String path) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
